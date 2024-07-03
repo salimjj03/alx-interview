57dbb65140d879ecc3e08d45475df62886cca07d
salimjj03 <salemaliyu1992@gmail.com>
2024-07-03 10:34:56 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,17 +4,17 @@ prime game
 """
 
 
-def is_prime(n):
+def is_prime(num):
     """Check if a number is prime."""
-    if n <= 1:
+    if num <= 1:
         return False
-    if n <= 3:
+    if num <= 3:
         return True
-    if n % 2 == 0 or n % 3 == 0:
+    if num % 2 == 0 or num % 3 == 0:
         return False
     i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
             return False
         i += 6
     return True
@@ -27,18 +27,14 @@ def isWinner(x, nums):
 
     Maria = 0
     Ben = 0
-    count = 0
 
     for round in range(x):
-        ls = [i for i in range(1, nums[round] + 1)]
-        for num in ls:
-            if is_prime(num):
-                count += 1
+        ls = [i for i in range(1, nums[round] + 1) if is_prime(i)]
+        count = len(ls)
         if count == 0 or count % 2 == 0:
             Ben += 1
         else:
             Maria += 1
-        count = 0
     if Maria == Ben:
         return (None)
     return("Maria" if Maria > Ben else "Ben")
